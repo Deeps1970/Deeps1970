e6f6b5569b088cfc701049ddd06aa0acb57a1eac
Deepak Thiagarajan <thiagarajandeepu2007@gmail.com>
2026-06-13 10:06:05 +0530

    Update portfolioAssets.ts
    + Added Certification Links
    + Added Logo's for Certification Organizations

--- src/data/portfolioAssets.ts
+++ src/data/portfolioAssets.ts
@@ -76,67 +76,52 @@ export const certificationAssets: Record<string, CertificationAsset> = {
   },
   "Stay Certified – Back-End Developer – ABAP Cloud": {
     title: "Stay Certified – Back-End Developer – ABAP Cloud",
-    certificate: "/__l5e/assets-v1/1efbccd2-c8e2-4aec-918c-5edb82311f4c/ABAP-Backend-Dev.pdf",
+    certificate: "https://www.dropbox.com/scl/fi/lyxet9waxgxge8m5usjit/ABAP-Backend-Dev.pdf?rlkey=xq4izt0pp37quk82su3md1oyj&st=la0chw78&dl=0",
     logo: "https://imgs.search.brave.com/vZdmXRO82pw6hxGZUxLg7-TMpMFK76f_0svmtby2FPg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nby53aW5lL2Ev/bG9nby9TQVBfU0Uv/U0FQX1NFLUxvZ28u/d2luZS5zdmc",
   },
   "Implementing Fund Accounting in SAP S/4HANA Cloud Public Edition": {
     title: "Implementing Fund Accounting in SAP S/4HANA Cloud Public Edition",
-    certificate: "/__l5e/assets-v1/363695df-7288-4761-bbee-d1f79ab215f3/Implementing_Fund_Accounting_in_SAP.jpg",
+    certificate: "https://www.dropbox.com/scl/fi/9m7vlqoc6hytwd43dkc4u/Implementing-Fund-Accounting-in-SAP.jpg?rlkey=p3zpuigw3fj4j1psvgq7r28lx&st=s4vo8qa2&dl=0",
     logo: "https://imgs.search.brave.com/vZdmXRO82pw6hxGZUxLg7-TMpMFK76f_0svmtby2FPg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bG9nby53aW5lL2Ev/bG9nby9TQVBfU0Uv/U0FQX1NFLUxvZ28u/d2luZS5zdmc",
   },
   "FlashBuild (International Pre-Conference Workshop)": {
     title: "FlashBuild (International Pre-Conference Workshop)",
-    certificate: "/__l5e/assets-v1/09467682-b088-469e-9777-d0f84567274b/flasbuild.jpg",
+    certificate: "https://www.dropbox.com/scl/fi/30w3mgpikrwy2p8xaoxuq/flasbuild.jpg?rlkey=lggnr66r0t98hloj3bokj4h5r&st=gsx63g8o&dl=0",
     logo: "https://imgs.search.brave.com/4iXB3flF0RscuZjDVeHQTMvkxRvV95ARKsITUb9zgxU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zZWVr/dmVjdG9ycy5jb20v/c3RvcmFnZS9pbWFn/ZXMvU3JtJTIwVW5p/dmVyc2l0eS0wNC5z/dmc",
   },
   "HackElite’26 – National Hackathon": {
     title: "HackElite’26 – National Hackathon",
-    certificate: "/__l5e/assets-v1/c02daa32-7e60-48bb-8987-de2a99676f64/hackelite26.jpeg",
+    certificate: "https://www.dropbox.com/scl/fi/0vuzdf52f17epc83vq57m/hackelite-26.jpeg?rlkey=ukp99l2369wcjn66m9jb2ehlk&st=euh6cn7q&dl=0",
     logo: "https://imgs.search.brave.com/4iXB3flF0RscuZjDVeHQTMvkxRvV95ARKsITUb9zgxU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zZWVr/dmVjdG9ycy5jb20v/c3RvcmFnZS9pbWFn/ZXMvU3JtJTIwVW5p/dmVyc2l0eS0wNC5z/dmc",
   },
   "HackForge’26 – National Hackathon": {
     title: "HackForge’26 – National Hackathon",
-    certificate: "/__l5e/assets-v1/6cd462dc-9678-47a6-be35-89c75dd75e39/hackforge26.pdf",
+    certificate: "https://www.dropbox.com/scl/fi/fen1x0l5rlwnsq5sck4n0/hackforge-26.pdf?rlkey=ch2tsep0szcfyootdzgxfor4v&st=gjva2pi0&dl=0",
     logo: "https://imgs.search.brave.com/4iXB3flF0RscuZjDVeHQTMvkxRvV95ARKsITUb9zgxU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zZWVr/dmVjdG9ycy5jb20v/c3RvcmFnZS9pbWFn/ZXMvU3JtJTIwVW5p/dmVyc2l0eS0wNC5z/dmc",
   },
   "Introduction to Artificial Intelligence": {
     title: "Introduction to Artificial Intelligence",
-    certificate: "/__l5e/assets-v1/c5a06943-d148-4213-ba8b-17e53ede35fb/intro-to-ai.pdf",
+    certificate: "https://www.dropbox.com/scl/fi/jm609i36whlvok1a5v50w/intro-to-ai.pdf?rlkey=ncg477emxs9c36yq8zopby4zh&st=avyoewzi&dl=0",
     logo: "https://imgs.search.brave.com/41GaSL9S0yda9ta7GqMiNmU7AIL3wfp78AG2zrxCH2U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/YnJhbmRmZXRjaC5p/by9pZGpLT1JGUUIt/L3cvMTY2Ny9oLzE2/NjcvdGhlbWUvZGFy/ay9pY29uLmpwZWc_/Yz0xYnhpZDY0TXVw/N2FjemV3U0FZTVgm/dD0xNzQ4Mjk1ODQ1/NTE0",
   },
   "Fintech Workshop": {
     title: "Fintech Workshop",
-    certificate: "/__l5e/assets-v1/aa8bd6d5-21ff-49a5-a77f-f50757a1d0df/fintech.pdf",
+    certificate: "https://www.dropbox.com/scl/fi/xh1p64dwxaogia9ka5jvj/fintech.pdf?rlkey=p7vxsibjet6pfs51g95khxook&st=uqpgscpz&dl=0",
     logo: "https://imgs.search.brave.com/4iXB3flF0RscuZjDVeHQTMvkxRvV95ARKsITUb9zgxU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zZWVr/dmVjdG9ycy5jb20v/c3RvcmFnZS9pbWFn/ZXMvU3JtJTIwVW5p/dmVyc2l0eS0wNC5z/dmc",
   },
   "Introduction to Programming in C": {
     title: "Introduction to Programming in C",
-    certificate: "/__l5e/assets-v1/e3d5f558-35fa-44bd-aae0-59a45abc9c09/intro-prog-c.pdf",
+    certificate: "https://www.dropbox.com/scl/fi/f2558kby7j9zdyvgx0mvb/intro-prog-c.pdf?rlkey=42k9aha6txmmyuqnehyn1qw3m&st=z5oeyppq&dl=0",
     logo: "https://www.google.com/s2/favicons?domain=nptel.ac.in&sz=128",
   },
   "Introduction to Computer Architecture": {
     title: "Introduction to Computer Architecture",
-    certificate: "/__l5e/assets-v1/ae8b91d5-e735-4784-915e-a6a68132488a/intro-to-computerarc.pdf",
+    certificate: "https://www.dropbox.com/scl/fi/nlb4kkiiu0b5lf0ks410d/intro-to-computerarc.pdf?rlkey=5e7y4zoi7o2od0l8hdtcxcw1t&st=y8fv2dx9&dl=0",
     logo: "https://imgs.search.brave.com/R0D2MowocQQ05_Gejlj-4i0rjFczQRVH7Q8kZ_Du3uk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9nYXRl/d2F5LmVkdS5pbi9p/bWFnZXMvbG9nb3Mv/aW5mb3N5cy1zcHJp/bmdib2FyZC53ZWJw",
   },
   "Introduction to Unix": {
     title: "Introduction to Unix",
-    certificate: "/__l5e/assets-v1/7f2b12c7-189a-406c-9e81-257ecd9ea66f/intro-to-unix.pdf",
+    certificate: "https://www.dropbox.com/scl/fi/6u4xwfqgpnv97n0y824tl/intro-to-unix.pdf?rlkey=7883lwe6vs3vdcch72r4eig3w&st=76qjaenj&dl=0",
     logo: "https://imgs.search.brave.com/R0D2MowocQQ05_Gejlj-4i0rjFczQRVH7Q8kZ_Du3uk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9nYXRl/d2F5LmVkdS5pbi9p/bWFnZXMvbG9nb3Mv/aW5mb3N5cy1zcHJp/bmdib2FyZC53ZWJw",
-  },
-  "MERN Stack Masterclass": {
-    title: "MERN Stack Masterclass",
-    certificate: "https://media.licdn.com/dms/image/v2/D4D2DAQGMLDMufjhwqw/profile-treasury-document-images_1280/B4DZgcsrktGkAY-/1/1752828133321?e=1782345600&v=beta&t=GouX4r9adhlXe69iiEsJw96XH_TvEgIcezImZgU7mx0",
-    logo: "https://www.dropbox.com/scl/fi/c0f8yx2tahwcqh8p0cknt/novitechresearchanddevelopment_logo.jpg?rlkey=fuf576o68yqey0d52drmiphk6&st=i1vi8q7i&dl=0",
-  },
-  "Full Stack Development Webinar": {
-    title: "Full Stack Development Webinar",
-    certificate: "https://www.dropbox.com/scl/fi/7fgfxjlkic8fxoat92ex6/FSWD-Webinar.Guvi-Participation.png?rlkey=y0cv2a0ujl5u90eh0etvc6plo&st=46wweeqf&dl=0",
-    logo: "https://www.google.com/s2/favicons?domain=guvi.in&sz=128",
-  },
-  "Git & GitHub Workshop": {
-    title: "Git & GitHub Workshop",
-    certificate: "https://media.licdn.com/dms/image/v2/D4D2DAQGpfHZ4iOHFyg/profile-treasury-image-shrink_800_800/B4DZgclOM6HsAY-/0/1752826178841?e=1781845200&v=beta&t=CqP3ceTN5-HXeix9AQSOWMBRfunSzeVgNt9BB4DFEmY",
-    logo: "https://www.dropbox.com/scl/fi/ihey9w7gtr9ze64ufrev0/5722.webp?rlkey=ztg8ikgq8dsepu73heob4ge5n&st=u7lm32dd&dl=0",
   },
 };
